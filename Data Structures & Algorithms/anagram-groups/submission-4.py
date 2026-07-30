73553a09de2195e6cdd74cab8016fb11e5c53cb8
Rahul Kailasa <38584670+rahulgit7@users.noreply.github.com>
2026-07-29 17:13:41 -0700

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,23 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        if len(strs) == 1:
+            return [strs]
+        ana_map ={}
+        for i in range(len(strs)):
+            freq_arr = [0] * 26
+            for j in strs[i]:
+                index = ord(j) - ord('a') 
+                freq_arr[index] = freq_arr[index] + 1
+            key = tuple(freq_arr)
+            if key in ana_map:
+                ana_map[key].append(strs[i])
+            else:
+                ana_map.setdefault((key), []).append(strs[i])
+        answer = [val for val in ana_map.values()]
+        return answer
+
+                # hamp[len(strs[i])] = hmap[len(strs[i])].append[strs[i]]
+        
+
+
+            
